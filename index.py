deb6a97cda6e1f17be0cde32b6053fb09509b7af
Areo-Joe <1047726015@qq.com>
2025-01-25 16:15:22 +0800

    feat: special token

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ with open("resources/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
 tokenizer = Tokenizer.from_text(raw_text)
-encoded = tokenizer.encode("you, -- and I.")
+encoded = tokenizer.encode("you, -- and I. I love you.")
 
 print(encoded)
 print(tokenizer.decode(encoded))
--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,7 @@
 import re
 
+UNKNOWN = "<|unknown|>"
+END_OF_TEXT = "<|end_of_text|>"
 
 class Tokenizer:
     def __init__(self, vocab: dict[str, int]):
@@ -9,6 +11,9 @@ class Tokenizer:
     def encode(self, text: str) -> list[int]:
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int else UNKNOWN for item in preprocessed
+        ]
         ids = [self.str_to_int[item] for item in preprocessed]
         return ids
 
@@ -23,6 +28,8 @@ class Tokenizer:
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
 
         all_words = sorted(set(preprocessed))
+
+        all_words.extend([END_OF_TEXT, UNKNOWN])
 
         vocab = {token: integer for integer, token in enumerate(all_words)}
 
